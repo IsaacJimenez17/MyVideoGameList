33d2ce086a7fe3f842d1faa9624bd70e89167b95
Isaac Jimenez <isaacjimenez1717@gmail.com>
2023-12-05 22:00:58 -0500

    Adding AWS Amplify and AWS Cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myvideogamelista3004a99": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
